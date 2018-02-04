639645f04030272ce4338c5cc21910ef5bbbdaf3
James L <silentmagician.1@gmail.com>
2018-02-04 16:24:10 +0000

    Added x_offset and y_offset setting on screen initialization

--- Screen.py
+++ Screen.py
@@ -8,3 +8,5 @@ class Screen():
 
     def _setup(self):
         self.res = (win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1))
+        self.x_offset = self.res[0] // 2
+        self.y_offset = self.res[1] // 2
--- tests/test_screen.py
+++ tests/test_screen.py
@@ -9,6 +9,14 @@ class ScreenTest(unittest.TestCase):
     def setUp(self):
         self.screen = Screen(1)
 
-    def test_screen_setup(self):
+    def test_setup_sets_resolution(self):
         res = (win32api.GetSystemMetrics(0), win32api.GetSystemMetrics(1))
         self.assertEqual(self.screen.res, res)
+
+    def test_setup_sets_x_offset(self):
+        x_offset = win32api.GetSystemMetrics(0) // 2
+        self.assertEqual(self.screen.x_offset, x_offset)
+
+    def test_setup_sets_y_offset(self):
+        y_offset = win32api.GetSystemMetrics(1) // 2
+        self.assertEqual(self.screen.y_offset, y_offset)
