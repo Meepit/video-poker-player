ef7f25d316ad1a2540a4732b791278fe3453a114
James L <silentmagician.1@gmail.com>
2018-02-11 17:24:10 +0000

    Hand can determine a cards rank using OCR

--- Hand.py
+++ Hand.py
@@ -1,3 +1,6 @@
+import pytesseract
+
+
 class Hand:
     def __init__(self, screen):
         self.screen = screen
@@ -18,3 +21,19 @@ class Hand:
     def get_card_suit(self, card_num):
         return self.cards["card{0}".format(card_num)]["suit"]
 
+    def determine_rank(self, card_num, ocr=pytesseract):
+        # Rank makes up about 27% of the card, suit makes up about 25%
+        x1, y1, x2, y2 = self.get_card_coord(card_num)
+        rank_offset = int((y2 - y1) * 0.27)
+        rank = self.screen.image_grabber.grab((x1, y1, x1 + rank_offset, y1 + rank_offset))
+        rank = rank.convert("1")  # Convert to black/white for better detection.
+        rank_str = ocr.image_to_string(rank, config='-psm 10000 -c tessedit_char_whitelist=0123456789JQKA')
+        if rank_str == "10":
+            rank_str = "T"
+        if rank_str == "0":
+            rank_str = "Q"
+        if len(rank_str) == 0 or len(rank_str) > 1:
+            print("There was a problem detecting rank. Detected as {0}. Retrying".format(rank_str))
+            #time.sleep(1)
+            return self.determine_rank(card_num)
+        return rank_str
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -1,16 +1,20 @@
 from hand import Hand
+from screen import Screen
 import unittest
 from unittest import mock
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 
 class HandTest(unittest.TestCase):
     def setUp(self):
         screen = MagicMock()
-        self.cards = {"card1": {"coord": (0, 0, 0, 0)}}
+        self.img = MagicMock()
+        screen.screen_grabber.grab.return_value = self.img
+        self.cards = {"card1": {"coord": (0, 0, 5, 5)}}
         screen.screen.return_value = 1
         screen.build_locations.return_value = self.cards
         self.hand = Hand(screen)
+        self.ocr = MagicMock()
 
     def test_get_card_coord(self):
         self.assertEqual(self.hand.get_card_coord(1), self.cards["card1"]["coord"])
@@ -30,4 +34,9 @@ class HandTest(unittest.TestCase):
     def test_get_card_suit(self):
         self.hand.set_card_rank(1, "H")
         self.assertEqual(self.hand.get_card_rank(1), "H")
+
+    def test_determine_rank(self):
+        self.img.convert.return_value = True
+        self.ocr.image_to_string.return_value = "5"
+        self.assertEqual(self.hand.determine_rank(1, ocr=self.ocr), "5")
 
