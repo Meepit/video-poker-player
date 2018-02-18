f9b540b51d9f7878dd8024cafd4bba7637ec7183
James L <silentmagician.1@gmail.com>
2018-02-18 16:21:42 +0000

    Added additional tests for suit detection

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -14,7 +14,7 @@ class HandTest(unittest.TestCase):
         screen.screen.return_value = 1
         screen.build_locations.return_value = self.cards
         self.os = MagicMock()
-        self.suits = {"heart": 170, "diamond": 180}
+        self.suits = {"heart": 170, "diamond": 180, "club": 164, "spade": 160}
         self.hand = Hand(screen, self.suits, self.os)
         self.ocr = MagicMock()
 
@@ -52,8 +52,24 @@ class HandTest(unittest.TestCase):
         self.ocr.image_to_string.return_value = "0"
         self.assertEqual(self.hand.determine_rank(1, ocr=self.ocr), "Q")
 
-    def test_determine_correct_suit(self):
+    def test_determine_correct_suit_H(self):
         self.img.getpixel.return_value = [170, 170, 170]
-        self.ocr.image_to_string.return_value = "S"
         self.os.path.getsize.return_value = 175
         self.assertEqual(self.hand.determine_suit(1), "H")
+
+    def test_determine_correct_suit_D(self):
+        self.img.getpixel.return_value = [170, 170, 170]
+        self.os.path.getsize.return_value = 179
+        self.assertEqual(self.hand.determine_suit(1), "D")
+
+    def test_determine_correct_suit_C(self):
+        self.hand.set_card_rank(1, "5")
+        self.img.getpixel.return_value = [164, 170, 170]
+        self.os.path.getsize.return_value = 164
+        self.assertEqual(self.hand.determine_suit(1), "C")
+
+    def test_determine_correct_suit_S(self):
+        self.hand.set_card_rank(1, "5")
+        self.img.getpixel.return_value = [161, 170, 170]
+        self.os.path.getsize.return_value = 161
+        self.assertEqual(self.hand.determine_suit(1), "S")
